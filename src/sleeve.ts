ff25fc3d27e28cafb7aa3232616b4aae1a53ce5e
Adrian Vargo <adrian.vargo@gmail.com>
2023-07-26 07:52:30 -0400

    SLeeve training and on exit updates

--- src/sleeve.ts
+++ src/sleeve.ts
@@ -3,6 +3,12 @@ import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
 	ns.disableLog("ALL");
 
+	ns.atExit(() => {
+		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
+			ns.sleeve.setToIdle(i);
+		}
+	});
+
 	while (true) {
 		ns.clearLog();
 		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
@@ -19,9 +25,9 @@ export async function main(ns: NS): Promise<void> {
 				continue;
 			}
 
-			const lowestStat = Math.min(sleeve.skills.strength, sleeve.skills.defense, sleeve.skills.dexterity, sleeve.skills.agility);
+			const lowestStat = Math.min(sleeve.skills.strength, sleeve.skills.defense, sleeve.skills.dexterity, sleeve.skills.agility, sleeve.skills.charisma);
 			if (lowestStat == sleeve.skills.hacking) {
-				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Study Computer Science");
+				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Computer Science");
 				ns.print("Training Hacking ", i);
 			} else if (lowestStat == sleeve.skills.strength) {
 				ns.sleeve.setToGymWorkout(i, "Powerhouse Gym", "Strength");
@@ -35,6 +41,9 @@ export async function main(ns: NS): Promise<void> {
 			} else if (lowestStat == sleeve.skills.agility) {
 				ns.sleeve.setToGymWorkout(i, "Powerhouse Gym", "Agility");
 				ns.print("Training Agility ", i);
+			} else if (lowestStat == sleeve.skills.charisma) {
+				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Leadership");
+				ns.print("Training Charisma ", i);
 			}
 
 			// if (sleeve.skills.hacking < 250 && sleeve.city == "Sector-12") {
