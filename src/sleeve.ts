698e5bc235cb90c21a9bd3c768bf9792e6a9792f
Adrian Vargo <adrian.vargo@gmail.com>
2023-07-26 16:23:10 -0400

    Update sleeve

--- src/sleeve.ts
+++ src/sleeve.ts
@@ -5,6 +5,19 @@ export async function main(ns: NS): Promise<void> {
 
 	ns.atExit(() => {
 		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
+			const sleeve = ns.sleeve.getSleeve(i);
+			if (sleeve.shock > 0) {
+				ns.sleeve.setToShockRecovery(i);
+				ns.print("Shock Recovery ", i);
+				continue;
+			}
+
+			if (sleeve.sync < 100) {
+				ns.sleeve.setToSynchronize(i);
+				ns.print("Synchronize ", i);
+				continue;
+			}
+
 			ns.sleeve.setToIdle(i);
 		}
 	});
@@ -25,9 +38,12 @@ export async function main(ns: NS): Promise<void> {
 				continue;
 			}
 
+			// if (task?.type != "CRIME") ns.sleeve.setToCommitCrime(i, "Larceny");
+			// continue;
+
 			const lowestStat = Math.min(sleeve.skills.strength, sleeve.skills.defense, sleeve.skills.dexterity, sleeve.skills.agility, sleeve.skills.charisma);
 			if (lowestStat == sleeve.skills.hacking) {
-				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Computer Science");
+				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Study Computer Science");
 				ns.print("Training Hacking ", i);
 			} else if (lowestStat == sleeve.skills.strength) {
 				ns.sleeve.setToGymWorkout(i, "Powerhouse Gym", "Strength");
@@ -45,18 +61,6 @@ export async function main(ns: NS): Promise<void> {
 				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Leadership");
 				ns.print("Training Charisma ", i);
 			}
-
-			// if (sleeve.skills.hacking < 250 && sleeve.city == "Sector-12") {
-			// 	if (task?.type != "CLASS") ns.sleeve.setToUniversityCourse(i, "Rothman University", "Study Computer Science");
-			// 	continue;
-			// } else if (sleeve.skills.hacking < 250 && sleeve.city == "Volhaven") {
-			// 	if (task?.type != "CLASS") ns.sleeve.setToUniversityCourse(i, "ZB Institute of Technology", "Study Computer Science");
-			// 	continue;
-			// } else {
-			// 	// if (task?.type != "CRIME") ns.sleeve.setToCommitCrime(i, "Homicide");
-			// 	if (task?.type != "FACTION") ns.sleeve.setToFactionWork(i, "Sector-12", "security");
-			// 	continue;
-			// }
 		}
 		await ns.sleep(10);
 	}
