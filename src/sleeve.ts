7aa1a05a69dfb138d10d802aa21b4c16345e0aaf
Adrian Vargo <adrian.vargo@gmail.com>
2023-07-29 10:25:24 -0400

    Set sleeves to commit homicide if karma higher than -54000

--- src/sleeve.ts
+++ src/sleeve.ts
@@ -24,27 +24,24 @@ export async function main(ns: NS): Promise<void> {
 
 	while (true) {
 		ns.clearLog();
-		for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
+		const numberOfSleeves = 8;
+		for (let i = 0; i < numberOfSleeves; i++) {
 			const sleeve = ns.sleeve.getSleeve(i);
-			const task = ns.sleeve.getTask(i);
 
 			if (sleeve.shock > 0) {
-				if (task?.type != "RECOVERY") ns.sleeve.setToShockRecovery(i);
+				ns.sleeve.setToShockRecovery(i);
 				ns.print("Shock Recovery ", i);
 				continue;
 			} else if (sleeve.sync < 100) {
-				if (task?.type != "SYNCHRO") ns.sleeve.setToSynchronize(i);
+				ns.sleeve.setToSynchronize(i);
 				ns.print("Synchronize ", i);
 				continue;
 			}
 
-			// if (task?.type != "CRIME") ns.sleeve.setToCommitCrime(i, "Larceny");
-			// continue;
-
-			const lowestStat = Math.min(sleeve.skills.strength, sleeve.skills.defense, sleeve.skills.dexterity, sleeve.skills.agility, sleeve.skills.charisma);
-			if (lowestStat == sleeve.skills.hacking) {
-				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Study Computer Science");
-				ns.print("Training Hacking ", i);
+			const lowestStat = Math.min(sleeve.skills.strength, sleeve.skills.defense, sleeve.skills.dexterity, sleeve.skills.agility);
+			if (lowestStat > 49 && ns.heart.break() > -54000) {
+				ns.sleeve.setToCommitCrime(i, "Homicide");
+				ns.print("Commiting Homicide ", i);
 			} else if (lowestStat == sleeve.skills.strength) {
 				ns.sleeve.setToGymWorkout(i, "Powerhouse Gym", "Strength");
 				ns.print("Training Strength ", i);
@@ -57,10 +54,15 @@ export async function main(ns: NS): Promise<void> {
 			} else if (lowestStat == sleeve.skills.agility) {
 				ns.sleeve.setToGymWorkout(i, "Powerhouse Gym", "Agility");
 				ns.print("Training Agility ", i);
-			} else if (lowestStat == sleeve.skills.charisma) {
-				ns.sleeve.setToUniversityCourse(i, "Rothman University", "Leadership");
-				ns.print("Training Charisma ", i);
 			}
+			// } else if (lowestStat == sleeve.skills.charisma) {
+			// 	ns.sleeve.setToUniversityCourse(i, "Rothman University", "Leadership");
+			// 	ns.print("Training Charisma ", i);
+			// }
+			// } else if (lowestStat == sleeve.skills.hacking) {
+			// 		ns.sleeve.setToUniversityCourse(i, "Rothman University", "Study Computer Science");
+			// 		ns.print("Training Charisma ", i);
+			// }
 		}
 		await ns.sleep(10);
 	}
