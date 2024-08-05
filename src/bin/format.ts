beee5050a0d648b04f331f70fe28c042d1c463e1
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-05 06:45:01 -0400

    clean

--- src/bin/format.ts
+++
@@ -1,7 +0,0 @@
-import { NS } from "@ns";
-
-export async function main(ns: NS): Promise<void> {
-	const hostname = <string>ns.args[0] ?? "home";
-	const files = ns.ls(hostname, ".js").concat(ns.ls(hostname, ".dat"));
-	files.forEach((fileName) => ns.rm(fileName, hostname));
-}
--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const reservedHomeRam = 16;
-export const hackScripts = ["bin.wk.js", "bin.hk.js", "bin.gr.js"];
