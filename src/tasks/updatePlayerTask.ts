b617c351e153d70d7cf86af7068ecd22a278d2c1
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-05 23:53:03 -0400

    updated tashs to use enums for storename

--- src/tasks/updatePlayerTask.ts
+++ src/tasks/updatePlayerTask.ts
@@ -1,7 +1,8 @@
 import { DynamicScript, getDynamicScriptContent } from "/lib/system";
 import { ScriptTask } from "/lib/models";
+import { DatabaseStoreName } from "/lib/database";
 
 export default (taskName: string = 'UpdatePlayer') => new ScriptTask(
     { name: taskName, priority: 100, lastRun: 0, interval: 500, enabled: true },
-    new DynamicScript(taskName, getDynamicScriptContent('ns.getPlayer', '{...ns.getPlayer(), hasTorRouter: ns.hasTorRouter()}', 'ns_data')),
+    new DynamicScript(taskName, getDynamicScriptContent('ns.getPlayer', '{...ns.getPlayer(), hasTorRouter: ns.hasTorRouter()}', DatabaseStoreName.NS_Data)),
 )
--- src/tasks/updateResetInfoTask.ts
+++ src/tasks/updateResetInfoTask.ts
@@ -1,7 +1,8 @@
 import { DynamicScript, getDynamicScriptContent } from "/lib/system";
 import { ScriptTask } from "/lib/models";
+import { DatabaseStoreName } from "/lib/database";
 
 export default (taskName: string = 'UpdateResetInfo') => new ScriptTask(
     { name: taskName, priority: 100, lastRun: 0, interval: 2000, enabled: true },
-    new DynamicScript(taskName, getDynamicScriptContent('ns.getResetInfo', 'ns.getResetInfo()', 'ns_data'))
+    new DynamicScript(taskName, getDynamicScriptContent('ns.getResetInfo', 'ns.getResetInfo()', DatabaseStoreName.NS_Data))
 )
