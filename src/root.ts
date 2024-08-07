1d834df24c6805ade10ff7fe8481cee08acb8150
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 00:40:39 -0400

    added root server script

--- src/root.ts
+++ src/root.ts
@@ -1,7 +1,20 @@
 import { NS } from "@ns";
+import { Database } from "/lib/database";
+import { IScriptServer, ScriptServer } from "./lib/models";
 
 export async function main(ns: NS): Promise<void> {
-    const hostname = '';
-    try{ ns.brutessh(hostname); ns.ftpcrack(hostname); ns.relaysmtp(hostname); ns.httpworm(hostname); ns.sqlinject(hostname); } catch(e){}
-    try{ ns.nuke(hostname); } catch(e){}
+    const database = await Database.getInstance();
+    await database.open();
+
+    const hostname = ns.args[0] as string;
+
+    try { ns.brutessh(hostname); ns.ftpcrack(hostname); ns.relaysmtp(hostname); ns.httpworm(hostname); ns.sqlinject(hostname); } catch (e) { }
+    try {
+        ns.nuke(hostname);
+        const home = new ScriptServer(await database.get<IScriptServer>('servers', ''));
+        const target = new ScriptServer(await database.get<IScriptServer>('servers', hostname));
+
+        ns.scp(home.files.filter(f => f.startsWith('remote/')), hostname);
+        ns.exec('remote/hwhw.prep.js', hostname, Math.floor(target.ram.available / 2.3));
+    } catch (e) { }
 }
