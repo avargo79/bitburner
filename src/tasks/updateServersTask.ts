07a3f4437ff2324a29d8209c9d6303791094c941
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 01:08:08 -0400

    included hack prop in update servers task

--- src/tasks/updateServersTask.ts
+++ src/tasks/updateServersTask.ts
@@ -1,10 +1,10 @@
 import { DynamicScript } from "/lib/system";
 import { ScriptTask } from "/lib/models";
 
-export default (taskName: string = 'UpdatePServers') => new ScriptTask(
+export default (taskName: string = 'UpdateServers') => new ScriptTask(
     { name: taskName, priority: 100, lastRun: 0, interval: 500, enabled: true },
     new DynamicScript(taskName,
-        'getServerList(ns).map(server => ({ ...ns.getServer(server), files: ns.ls(server), pids: ns.ps(server)})).forEach(server => database.saveRecord("servers", server))',
+        'getServerList(ns).map(server => ({ hack: {hkTime: -1, wkTime: -1, grTime: -1}, ...ns.getServer(server), files: ns.ls(server), pids: ns.ps(server)})).forEach(server => database.saveRecord("servers", server))',
         ['import { getServerList } from "/lib/network";']
     )
 )
