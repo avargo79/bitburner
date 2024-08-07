713a407540ebb0c136b861b57578f0e2e61901cc
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 19:44:52 -0400

    added targeted root script with loop hack function

--- src/root.ts
+++ src/root.ts
@@ -1,6 +1,10 @@
 import { NS } from "@ns";
-import { Database } from "/lib/database";
-import { IScriptServer, ScriptServer } from "./lib/models";
+import { Database, DatabaseStoreName } from "/lib/database";
+import { IScriptServer } from "/lib/models";
+
+export function autocomplete(data: { servers: any }, args: any) {
+    return data.servers;
+}
 
 export async function main(ns: NS): Promise<void> {
     const database = await Database.getInstance();
@@ -11,10 +15,15 @@ export async function main(ns: NS): Promise<void> {
     try { ns.brutessh(hostname); ns.ftpcrack(hostname); ns.relaysmtp(hostname); ns.httpworm(hostname); ns.sqlinject(hostname); } catch (e) { }
     try {
         ns.nuke(hostname);
-        const home = new ScriptServer(await database.get<IScriptServer>('servers', ''));
-        const target = new ScriptServer(await database.get<IScriptServer>('servers', hostname));
 
-        ns.scp(home.files.filter(f => f.startsWith('remote/')), hostname);
-        ns.exec('remote/hwhw.prep.js', hostname, Math.floor(target.ram.available / 2.3));
     } catch (e) { }
+
+    await ns.sleep(1000);
+    const remoteHost = await database.get<IScriptServer>('servers', hostname);
+
+    if (remoteHost.hasAdminRights) {
+        const homeServer = await database.get<IScriptServer>(DatabaseStoreName.Servers, 'home');
+        ns.scp(homeServer.files.filter(f => f.startsWith('remote/') || f.startsWith('lib/')), hostname);
+        ns.exec('remote/wgh.loop.js', hostname, Math.floor((remoteHost.maxRam - remoteHost.ramUsed) / 2), hostname);
+    }
 }
