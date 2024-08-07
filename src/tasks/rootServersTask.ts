227ee6f6325f059c8633594a4942f56f9d15410d
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 00:39:18 -0400

    add remote prep script and updated root server task

--- src/tasks/rootServersTask.ts
+++ src/tasks/rootServersTask.ts
@@ -3,6 +3,27 @@ import { ScriptTask } from "/lib/models";
 
 export default (taskName: string = 'RootServers') => new ScriptTask(
     { name: taskName, priority: 20, lastRun: 0, interval: 2000, enabled: true },
-    new DynamicScript(taskName, '(await database.getAll("servers")).filter(s => !s.hasAdminRights).map(s => s.hostname).forEach(s => { try{ ns.brutessh(s); ns.ftpcrack(s); ns.relaysmtp(s); ns.httpworm(s); ns.sqlinject(s); } catch(e){} try{ ns.nuke(s); } catch(e){} });'
-    )
+    new DynamicScript(taskName, `
+        const servers = await database.getAll("servers");
+        const targets = servers.filter(s => !s.hasAdminRights).map(s => s.hostname);
+        for (const hostname of targets) {
+            try{ 
+                ns.brutessh(hostname); 
+                ns.ftpcrack(hostname); 
+                ns.relaysmtp(hostname); 
+                ns.httpworm(hostname); 
+                ns.sqlinject(hostname); 
+            } catch(e){} 
+                
+            try{ 
+                ns.nuke(hostname); 
+
+                const home = await database.get('servers', 'home');
+                const target = await database.get('servers', hostname);
+                ns.scp(home.files.filter(f => f.startsWith('remote/')), hostname, 'home');
+                ns.scp(home.files.filter(f => f.startsWith('lib/')), hostname, 'home');
+                ns.exec('remote/hwhw.prep.js', hostname, Math.floor(target.ram.available / 1.9), hostname);
+            } catch(e){} 
+        }
+    `, [])
 )
