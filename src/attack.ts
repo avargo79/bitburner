79ff79fc9158da6701409871129a0465ba6633e2
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 00:40:51 -0400

    wip attack script

--- src/attack.ts
+++ src/attack.ts
@@ -6,6 +6,9 @@ import { ScriptServer, IScriptServer } from "/lib/models";
 // remote/hwgw.grow 1.75GB
 // remote/hwgw.hack 1.70GB
 export async function main(ns: NS): Promise<void> {
+    ns.disableLog("scp");
+    ns.clearLog();
+
     const database = await Database.getInstance();
     await database.open();
 
@@ -13,7 +16,8 @@ export async function main(ns: NS): Promise<void> {
     const homeServer = servers.find(s => s.hostname === 'home') as ScriptServer;
     const remoteFiles = homeServer.files.filter(f => f.startsWith('remote/'));
 
-    const attackers = servers.filter(s => (s.hasAdminRights && s.ram.max > 0) || s.purchasedByPlayer);
+    const attackers = servers.filter(s => s.isAttacker);
+    ns.print(`Found attackers: (${attackers.length}) [${attackers.map(a => a.hostname).reduce((acc, cur) => acc + cur + ', ', '')}]`);
     for (const attacker of attackers) {
         ns.scp(remoteFiles, attacker.hostname)
     }
@@ -27,13 +31,10 @@ export async function main(ns: NS): Promise<void> {
         let secDecrease = 1;
 
         while (true) {
-            secDecrease = ns.weakenAnalyze(weakenThreads)
-            if (weakenTarget.security.current - secDecrease > weakenTarget.security.min) {
-                weakenThreads += 5;
-                continue;
-            }
-            ns.tprint(`${weakenTarget.hostname} Sec: +${weakenTarget.security.current - weakenTarget.security.min}`);
-            ns.tprint(`Weakening ${weakenTarget.hostname} by ${secDecrease} with ${weakenThreads} threads.`);
+            secDecrease = weakenTarget.security.current - weakenTarget.security.min;
+            weakenThreads = Math.ceil(secDecrease * 20.5);
+            ns.print(`${weakenTarget.hostname} Sec: +${secDecrease}`);
+            ns.print(`Weakening ${weakenTarget.hostname} by ${secDecrease} with ${weakenThreads} threads.`);
             break;
         }
         const orgThreads = weakenThreads;
@@ -47,22 +48,25 @@ export async function main(ns: NS): Promise<void> {
             if (weakenThreads === 0) break;
         }
         if (weakenThreads > 0)
-            ns.tprint(`${weakenThreads} threads more needed.  should decrease by ${ns.weakenAnalyze(orgThreads - weakenThreads)} (${weakenTarget.security.current - weakenTarget.security.min - ns.weakenAnalyze(orgThreads - weakenThreads)})`);
+            ns.print(`${weakenThreads} threads more needed.`);
+
+        ns.print(`Should decrease by ${ns.weakenAnalyze(orgThreads - weakenThreads)} (+${secDecrease})`);
     }
 
-    const growTarget = needsPrep.find(s => s.money.max > s.money.current && s.security.current === s.security.min);
-    if (growTarget) {
-        let money = growTarget.money.max / growTarget.money.current;
-        if (money == Infinity) money = growTarget.money.max;
-        let growThreads = Math.ceil(ns.growthAnalyze(growTarget.hostname, money));
+    return;
+    // const growTarget = needsPrep.find(s => s.money.max > s.money.current && s.security.current === s.security.min);
+    // if (growTarget) {
+    //     let money = growTarget.money.max / growTarget.money.current;
+    //     if (money == Infinity) money = growTarget.money.max;
+    //     let growThreads = Math.ceil(ns.growthAnalyze(growTarget.hostname, money));
 
-        for (const attacker of attackers) {
-            const attackerAvailableThreads = Math.floor(attacker.ram.available / 1.75);
-            const threads = Math.min(growThreads, attackerAvailableThreads);
-            ns.exec('remote/hwgw.grow.js', attacker.hostname, threads, growTarget.hostname, 0);
-            growThreads -= threads;
+    //     for (const attacker of attackers) {
+    //         const attackerAvailableThreads = Math.floor(attacker.ram.available / 1.75);
+    //         const threads = Math.min(growThreads, attackerAvailableThreads);
+    //         ns.exec('remote/hwgw.grow.js', attacker.hostname, threads, growTarget.hostname, 0);
+    //         growThreads -= threads;
 
-            if (growThreads === 0) break;
-        }
-    }
+    //         if (growThreads === 0) break;
+    //     }
+    // }
 }
