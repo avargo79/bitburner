a6c0798c953347a1de967f67907d12e4d0152d73
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-06 22:33:04 -0400

    wip attack script

--- src/attack.ts
+++ src/attack.ts
@@ -2,6 +2,9 @@ import { NS } from "@ns";
 import { Database, DatabaseStoreName } from "/lib/database";
 import { ScriptServer, IScriptServer } from "/lib/models";
 
+// remote/hwgw.weaken 1.75GB
+// remote/hwgw.grow 1.75GB
+// remote/hwgw.hack 1.70GB
 export async function main(ns: NS): Promise<void> {
     const database = await Database.getInstance();
     await database.open();
@@ -10,49 +13,56 @@ export async function main(ns: NS): Promise<void> {
     const homeServer = servers.find(s => s.hostname === 'home') as ScriptServer;
     const remoteFiles = homeServer.files.filter(f => f.startsWith('remote/'));
 
-    const attackers = servers.filter(s => s.hasAdminRights && s.ram.max > 0);
+    const attackers = servers.filter(s => (s.hasAdminRights && s.ram.max > 0) || s.purchasedByPlayer);
     for (const attacker of attackers) {
         ns.scp(remoteFiles, attacker.hostname)
     }
 
     const targets = servers.filter(s => s.hasAdminRights && !s.purchasedByPlayer && s.money.max > 0);
-
     const needsPrep = targets.filter(t => t.needsPrep);
-    // const canAttack = targets.filter(t => !t.needsPrep);
 
-    for (const target of needsPrep) {
-        let attacker = new ScriptServer(await database.get<IScriptServer>(DatabaseStoreName.Servers, 'home'));
+    const weakenTarget = needsPrep.find(s => s.security.current > s.security.min);
+    if (weakenTarget) {
+        let weakenThreads = 1;
+        let secDecrease = 1;
 
-        // remote/hwgw.weaken 1.75GB
-        // remote/hwgw.grow 1.75GB
-        // remote/hwgw.hack 1.70GB
-        let weakenTheads = target.security.current > target.security.min
-            ? Math.ceil((target.security.current - target.security.min) * 20)
-            : 0;
-        weakenTheads = Math.min(weakenTheads, Math.floor(attacker.ram.available / 1.75));
-        if (weakenTheads > 0) {
-            ns.tprint(`Weakening: ${target.hostname}`);
-            ns.exec('remote/hwgw.weaken.js', attacker.hostname, weakenTheads, target.hostname, 0);
+        while (true) {
+            secDecrease = ns.weakenAnalyze(weakenThreads)
+            if (weakenTarget.security.current - secDecrease > weakenTarget.security.min) {
+                weakenThreads += 5;
+                continue;
+            }
+            ns.tprint(`${weakenTarget.hostname} Sec: +${weakenTarget.security.current - weakenTarget.security.min}`);
+            ns.tprint(`Weakening ${weakenTarget.hostname} by ${secDecrease} with ${weakenThreads} threads.`);
+            break;
         }
+        const orgThreads = weakenThreads;
 
-        attacker = new ScriptServer(await database.get<IScriptServer>(DatabaseStoreName.Servers, 'home'))
-        let money = target.money.max / target.money.current;
-        if (money == Infinity) money = target.money.max;
-        let growThreads = Math.ceil(ns.growthAnalyze(target.hostname, money));
-        growThreads = Math.min(growThreads, Math.floor(attacker.ram.available / 1.75));
-        if (growThreads > 0) {
-            ns.tprint(`Growing: ${target.hostname}`);
-            ns.exec('remote/hwgw.grow.js', attacker.hostname, growThreads, target.hostname, 0);
+        for (const attacker of attackers) {
+            const attackerAvailableThreads = Math.floor(attacker.ram.available / 1.75);
+            const threads = Math.min(weakenThreads, attackerAvailableThreads);
+            const pid = ns.exec('remote/hwgw.weaken.js', attacker.hostname, threads, weakenTarget.hostname, 0);
+            weakenThreads -= pid > 0 ? threads : 0;
+
+            if (weakenThreads === 0) break;
         }
-        //weakenTheads = Math.ceil(this.instance.hackAnalyzeSecurity(this.hackThreads, this.id)) + 10;
+        if (weakenThreads > 0)
+            ns.tprint(`${weakenThreads} threads more needed.  should decrease by ${ns.weakenAnalyze(orgThreads - weakenThreads)} (${weakenTarget.security.current - weakenTarget.security.min - ns.weakenAnalyze(orgThreads - weakenThreads)})`);
+    }
 
-        // return weakenTheads == Infinity ? 1 : weakenTheads;
-        // const needsPrep =
-        //     (target.moneyAvailable ?? 0) < (target.moneyMax ?? 0) ||
-        //     (target.hackDifficulty ?? 0) > (target.hackDifficulty ?? 0);
+    const growTarget = needsPrep.find(s => s.money.max > s.money.current && s.security.current === s.security.min);
+    if (growTarget) {
+        let money = growTarget.money.max / growTarget.money.current;
+        if (money == Infinity) money = growTarget.money.max;
+        let growThreads = Math.ceil(ns.growthAnalyze(growTarget.hostname, money));
 
-        // if() {
-        //     ns.run('attack.js', 1, target.hostname);
-        // }
+        for (const attacker of attackers) {
+            const attackerAvailableThreads = Math.floor(attacker.ram.available / 1.75);
+            const threads = Math.min(growThreads, attackerAvailableThreads);
+            ns.exec('remote/hwgw.grow.js', attacker.hostname, threads, growTarget.hostname, 0);
+            growThreads -= threads;
+
+            if (growThreads === 0) break;
+        }
     }
 }
