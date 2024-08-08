c211210d0e46a3dfcc3a07a43b6b566a5e343056
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-08 00:38:33 -0400

    Added kill all remote script

---
+++ src/killremote.ts
@@ -0,0 +1,20 @@
+import { NS } from "@ns";
+import { Database, DatabaseStoreName } from "/lib/database";
+import { IScriptServer } from "/lib/models";
+
+export async function main(ns: NS) {
+    ns.disableLog("ALL");
+    ns.clearLog();
+
+    const database = await Database.getInstance();
+    await database.open();
+
+    const allServers = await database.getAll<IScriptServer>(DatabaseStoreName.Servers);
+
+    const remotePIDs = allServers.filter(s => s.hostname !== 'home')
+        .flatMap(s => s.pids.filter(p => p.filename.startsWith("remote/")))
+        .map(pid => ({ hostname: pid.args[0], filename: pid.filename }));
+    for (const pid of remotePIDs) {
+        ns.print(`Killing ${pid.filename} on ${pid.hostname}... ${ns.scriptKill(pid.filename, pid.hostname) ? 'OK' : 'FAILED'}`);
+    }
+}
