591609aabcc6bb628c7efb8dff88698a2f8b23a5
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-08 00:38:53 -0400

    attack script cleanup

--- src/attack.ts
+++ src/attack.ts
@@ -4,16 +4,17 @@ import { IScriptPlayer, IScriptServer } from "/lib/models";
 import { Colors } from "./lib/consts";
 
 const reserveRam = 32
-
-// remote/hwgw.weaken 1.75GB
-// remote/hwgw.grow 1.75GB
-// remote/hwgw.hack 1.70GB
 export async function main(ns: NS): Promise<void> {
     ns.disableLog("ALL");
+    ns.clearLog();
 
     const database = await Database.getInstance();
     await database.open();
 
+    let player: IScriptPlayer;
+    let executingServer: IScriptServer;
+
+    const getTargetValue = (server: IScriptServer) => Math.floor(server.moneyMax! / server.hackData.wkTime);
     const isTarget = (server: IScriptServer) =>
         server.hasAdminRights
         && player.skills.hacking >= server.requiredHackingSkill!
@@ -28,77 +29,86 @@ export async function main(ns: NS): Promise<void> {
         isTarget(server)
         && !shouldWeaken(server)
         && !shouldGrow(server)
-        && homeServer.pids.find(p => p.filename === "remote/hwgw.hack" && p.args.includes(server.hostname)) === undefined;
+        && executingServer.pids.find(p => p.filename === "remote/hwgw.hack" && p.args.includes(server.hostname)) === undefined;
 
-    const refreshPlayer = async () => database.get<IScriptPlayer>(DatabaseStoreName.NS_Data, 'ns.getPlayer');
-    const refreshHomeServer = async () => database.get<IScriptServer>(DatabaseStoreName.Servers, 'home');
+    const sortByValue = (a: IScriptServer, b: IScriptServer) => getTargetValue(b) - getTargetValue(a)
+
+    const refreshPlayer = async () => await database.get<IScriptPlayer>(DatabaseStoreName.NS_Data, 'ns.getPlayer');
+    const refreshExecutingServer = async () => await database.get<IScriptServer>(DatabaseStoreName.Servers, 'home');
     const refreshServers = async (filterFn: (server: IScriptServer) => boolean) => (await database.getAll<IScriptServer>(DatabaseStoreName.Servers)).filter(filterFn);
-    const tryExec = async (script: string, hostname: string, threads: number) => {
-        const execServer = await refreshHomeServer();
-        const availableThreads = Math.floor((execServer.maxRam - execServer.ramUsed - reserveRam) / 2);
+
+    const tryExec = (executingServer: IScriptServer, script: string, hostname: string, threads: number) => {
+        const availableThreads = Math.floor(((executingServer.maxRam - reserveRam) - executingServer.ramUsed) / 2);
         const execThreads = Math.min(threads, availableThreads);
         if (execThreads <= 0) return 0;
-        ns.print(`\tTrying to exec '${script}' on ${hostname} with ${Colors.brightRed}${execThreads}${Colors.reset} threads(req=${Colors.brightBlue}${threads}${Colors.reset})`);
-        const pid = ns.exec(script, execServer.hostname, execThreads, hostname, 0);
+        ns.print(`${hostname}: Trying to exec '${script}' with ${Colors.brightRed}${execThreads}${Colors.reset} threads(req=${Colors.brightBlue}${threads}${Colors.reset})`);
+        const pid = ns.exec(script, executingServer.hostname, execThreads, hostname, 0);
         return pid > 0 ? execThreads : 0;
     };
+    const tryWeaken = (executingServer: IScriptServer, target: IScriptServer) => {
+        const executingThreads = executingServer.pids
+            .filter(p => p.filename === "remote/hwgw.weaken.js" && p.args.includes(target.hostname))
+            .reduce((acc, p) => acc + p.threads, 0);
+        const secDecrease = target.hackDifficulty! - target.minDifficulty!;
+        let weakenThreads = Math.ceil(secDecrease * 20) - executingThreads;
 
-    let player = await refreshPlayer();
-    let homeServer = await refreshHomeServer();
+        weakenThreads -= tryExec(executingServer, 'remote/hwgw.weaken.js', target.hostname, weakenThreads);
+    }
+    const tryGrow = (executingServer: IScriptServer, target: IScriptServer) => {
+        const money = target.moneyMax! / target.moneyAvailable!;
+        const executingThreads = executingServer.pids
+            .filter(p => p.filename === "remote/hwgw.grow.js" && p.args.includes(target.hostname))
+            .reduce((acc, p) => acc + p.threads, 0);
+        let growThreads = Math.ceil(ns.growthAnalyze(target.hostname, money == Infinity ? target.moneyMax! : money)) - executingThreads;
+        growThreads -= tryExec(executingServer, 'remote/hwgw.grow.js', target.hostname, growThreads);
+    }
+    const tryHack = (executingServer: IScriptServer, target: IScriptServer) => {
+        const executingThreads = executingServer.pids
+            .filter(p => p.filename === "remote/hwgw.hack.js" && p.args.includes(target.hostname))
+            .reduce((acc, p) => acc + p.threads, 0);
+        let hackThreads = Math.ceil(ns.hackAnalyzeThreads(target.hostname, target.moneyMax!)) - executingThreads;
+        if (hackThreads == Infinity) hackThreads = 1;
+
+        hackThreads -= tryExec(executingServer, 'remote/hwgw.hack.js', target.hostname, hackThreads);
+    }
 
     while (true) {
         ns.clearLog();
         player = await refreshPlayer();
-        homeServer = await refreshHomeServer();
 
+        // Weaken targets
+        executingServer = await refreshExecutingServer();
         const weakenTargets = await refreshServers(shouldWeaken);
-        weakenTargets.sort((a, b) => a.hackData.wkTime - b.hackData.wkTime);
-        for (const target of weakenTargets) {
-            ns.print(`WEAKEN: ${target.hostname} Sec: ${target.hackDifficulty} Money: ${formatMoney(target.moneyAvailable!)}/${formatMoney(target.moneyMax!)}`);
+        weakenTargets.sort(sortByValue);
+        weakenTargets.forEach(target => tryWeaken(executingServer, target));
+        await ns.sleep(1000);
 
-            let secDecrease = target.hackDifficulty! - target.minDifficulty!;
-            let weakenThreads = Math.ceil(secDecrease * 20);
-
-            weakenThreads -= await tryExec('remote/hwgw.weaken.js', target.hostname, weakenThreads);
-            await ns.sleep(1000);
-            ns.print("\n");
-            if (weakenThreads === 0) break;
-        }
-
+        // Grow targets
+        executingServer = await refreshExecutingServer();
         const growTargets = await refreshServers(shouldGrow);
-        growTargets.sort((a, b) => a.hackData.grTime - b.hackData.grTime);
-        for (const target of growTargets) {
-            ns.print(`GROW: ${target.hostname} Sec: ${target.hackDifficulty} Money: ${formatMoney(target.moneyAvailable!)}/${formatMoney(target.moneyMax!)}`);
+        growTargets.sort(sortByValue);
+        growTargets.forEach(target => tryGrow(executingServer, target));
+        await ns.sleep(1000);
 
-            let money = target.moneyMax! / target.moneyAvailable!;
-            let growThreads = Math.ceil(ns.growthAnalyze(target.hostname, money == Infinity ? target.moneyMax! : money));
-
-            growThreads -= await tryExec('remote/hwgw.grow.js', target.hostname, growThreads);
+        // Hack targets if no weaken/grow targets
+        if (weakenTargets.length === 0 && growTargets.length === 0) {
+            executingServer = await refreshExecutingServer();
+            const hackTargets = await refreshServers(shouldHack);
+            hackTargets.sort(sortByValue);
+            hackTargets.forEach(target => tryHack(executingServer, target));
             await ns.sleep(1000);
-            ns.print("\n");
-            if (growThreads === 0) break;
         }
 
-        const hackTargets = await refreshServers(shouldHack);
-        hackTargets.sort((a, b) => a.hackData.hkTime - b.hackData.hkTime);
-        for (const target of hackTargets) {
-            ns.print(`HACK: ${target.hostname} Money: ${formatMoney(target.moneyMax!)}`);
-
-            let hackThreads = Math.ceil(ns.hackAnalyzeThreads(target.hostname, target.moneyMax!));
-            if (hackThreads == Infinity) hackThreads = 1;
-
-            hackThreads -= await tryExec('remote/hwgw.hack.js', target.hostname, hackThreads);
+        // Wait until threads available
+        let sleepingUntilThreads = (executingServer.maxRam - executingServer.ramUsed - reserveRam) / 2 < 1
+        while (sleepingUntilThreads) {
+            executingServer = await refreshExecutingServer();
+            sleepingUntilThreads = (executingServer.maxRam - executingServer.ramUsed - reserveRam) / 2 < 1;
+            const maxRam = executingServer.maxRam - reserveRam;
+            const availableRam = maxRam - executingServer.ramUsed;
+            ns.print(`Sleeping until threads available. Current RAM: ${Colors.brightRed}${ns.formatRam(availableRam)}/${ns.formatRam(maxRam)}${Colors.reset}`);
             await ns.sleep(1000);
-            ns.print("\n");
-            if (hackThreads === 0) break;
         }
-
-        let sleepingUntilThreads = true
-        do {
-            const server = await refreshHomeServer();
-            sleepingUntilThreads = (server.maxRam - server.ramUsed - reserveRam) / 2 < 1;
-            await ns.sleep(20000);
-        } while (sleepingUntilThreads);
     }
 }
 
