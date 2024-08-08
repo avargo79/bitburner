959cd502097d02810f313b06287e3749e29b37a8
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-07 21:07:10 -0400

    Refactored SolveContracts
    Setting required ram to 32
    Thinking the max ram used for this is now 14.2GB at any one time. With
    daemon.js(3.1GB) running the min ram needed is 32

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -14,7 +14,6 @@ export interface IScriptContract {
     file: string;
     hostname: string;
     data: string;
-    numTriesRemaining: number;
     solved: boolean;
 }
 
--- src/tasks/solveContractsTask.ts
+++ src/tasks/solveContractsTask.ts
@@ -4,71 +4,75 @@ import { ScriptTask } from "/lib/models";
 export default (taskName: string = 'SolveContracts') => new ScriptTask(
     { name: taskName, priority: 80, lastRun: 0, interval: 5000, enabled: false },
     new DynamicScript(taskName, `
-        // Require at least 64GB of RAM
-        if((await database.get("servers", ns.getHostname())).maxRam < 64) {
-            ns.toast("Server does not have enough RAM to run the contract task.");
+        //Require at least 32GB of RAM
+        const requiredRam = 32;
+        if((await database.get("servers", "home")).maxRam < requiredRam) {
+            ns.toast("Server does not have enough RAM(" + requiredRam + ") to run the contract task.");
             return;
         }
 
-        // update contracts
-        await new DynamicScript('SolveContracts_Update', \`
-            const servers = await database.getAll("servers");
-            servers.filter(s => s.files.some(f => f.endsWith(".cct")))
-                .forEach(async server => {
-                    server.files.filter(f => f.endsWith(".cct"))
-                        .forEach(async file => {
-                            const contractId = server.hostname + "-" + file;
-                            if (ns.ls(server.hostname, ".cct").includes(file)) {
-                                const contract = {
-                                    id: contractId,
-                                    solved: false,
-                                    file: file,
-                                    hostname: server.hostname,
-                                    type: ns.codingcontract.getContractType(file, server.hostname)
-                                };
-                                await database.saveRecord("contracts", contract);
-                            }
-                        });
-                });
-        \`, ['import { DynamicScript } from "/lib/system";']).run(ns, true);
+        const servers = await database.getAll("servers");
+        const contracts = [];
+        const contractFiles = servers.flatMap(s => s.files.filter(f => f.endsWith(".cct")).map(file => ({ hostname: s.hostname, file })));
+        ns.print("Found " + contractFiles.length + " contracts");
+        for (const contractFile of contractFiles) {
+            ns.print("Processing contract: " + contractFile.file);
 
-        // update contracts data
-        await new DynamicScript('SolveContracts_UpdateData', \`
-            const contracts = await database.getAll("contracts");
-            contracts.filter(contract => ns.ls(contract.hostname, ".cct").includes(contract.file))
-                .map(contract => ({ 
-                    ...contract, 
-                    data:  JSON.stringify(ns.codingcontract.getData(contract.file, contract.hostname)) 
-                })) 
-                .forEach(contract => database.saveRecord("contracts", contract));
-        \`, ['import { DynamicScript } from "/lib/system";']).run(ns, true);
+            // Get Contract Type
+            await (new DynamicScript(
+                contractFile.file + "-Type",
+                getDynamicScriptContent(
+                    "ns.codingcontract.getContractType",
+                    "ns.codingcontract.getContractType('" + contractFile.file + "', '" + contractFile.hostname + "')",
+                    DatabaseStoreName.NS_Data
+                )
+            )).run(ns, true);
+            const contractType = await database.get(DatabaseStoreName.NS_Data, "ns.codingcontract.getContractType");
+            
+            // Get Contract Data
+            await (new DynamicScript(
+                contractFile.file + "-Data",
+                getDynamicScriptContent(
+                    "ns.codingcontract.getData",
+                    "JSON.stringify(ns.codingcontract.getData('" + contractFile.file + "', '" + contractFile.hostname + "'))",
+                    DatabaseStoreName.NS_Data
+                )
+            )).run(ns, true);
+            const contractData = await database.get(DatabaseStoreName.NS_Data, "ns.codingcontract.getData");
 
-        // update contracts data
-        await new DynamicScript('SolveContracts_Solve', \`
-            const contracts = (await database.getAll('contracts'))
-                .filter(contract => contract.type in solvers &&contract.data !== undefined && ns.ls(contract.hostname, '.cct').includes(contract.file));
 
-            for (const contract of contracts) {
-                const data = JSON.parse(contract.data);
-                const solveFn = solvers[contract.type];
-                if (solveFn !== undefined) {
-                    const answer = solvers[contract.type](data);
-                    const reward = ns.codingcontract.attempt(answer, contract.file, contract.hostname);
-                    if (reward) {
-                        ns.toast('Contract rewarded: ' + reward);
-                        const solvedContract = await database.get('contracts', contract.id);
-                        solvedContract.solved = true;
-                        await database.saveRecord('contracts', solvedContract);
-                    }
+            const contract = {
+                id: contractFile.hostname + "-" + contractFile.file,
+                hostname: contractFile.hostname,
+                file: contractFile.file,
+                type: contractType,
+                data: contractData,
+                solved: false
+            }
+            await database.saveRecord("contracts", contract);
+            contracts.push(contract);
+        }
+
+        for (const contract of contracts) {
+            const data = JSON.parse(contract.data);
+            const solveFn = solvers[contract.type];
+            if (solveFn !== undefined) {
+                const answer = solvers[contract.type](data);
+                await (new DynamicScript(
+                    contract.file + "-Solve",
+                    getDynamicScriptContent(
+                        "ns.codingcontract.attempt",
+                        "ns.codingcontract.attempt('" + answer +"', '" + contract.file + "', '" + contract.hostname + "')",
+                        DatabaseStoreName.NS_Data
+                    )
+                )).run(ns, true);
+                const reward = await database.get(DatabaseStoreName.NS_Data, "ns.codingcontract.attempt");
+                if (reward) {
+                    ns.toast("Contract rewarded: " + reward);
+                    // await database.deleteRecord("contracts", contract.id);
+                    await database.saveRecord("contracts", { ...contract,solved: true });
                 }
             }
-        \`, ['import solvers from "/lib/contracts";', 'import { DynamicScript } from "/lib/system";'])
-        .run(ns, true);
-
-        // delete contract records that are no longer valid
-        (await database.getAll("contracts"))
-            .filter(contract => ns.ls(contract.hostname, ".cct")
-            .includes(contract.file) === false)
-            .forEach(contract => database.deleteRecord("contracts", contract.id));
-    `, ['import { DynamicScript } from "/lib/system";'])
-)
+        }
+    `, ['import { DynamicScript, getDynamicScriptContent } from "/lib/system";', 'import solvers from "/lib/contracts";']
+    ));
