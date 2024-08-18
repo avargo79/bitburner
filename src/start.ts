1f24d01b2b69586e77712530cae4eea083587aa8
Adrian Vargo <adrian.vargo@gmail.com>
2024-08-18 18:18:18 -0400

    fix start
    clear databases and set max-power on purchased servers

--- src/start.ts
+++ src/start.ts
@@ -1,29 +1,27 @@
 import { NS } from "@ns";
 import { Database, DatabaseStoreName } from "/lib/database";
 import { IScriptResetInfo } from "/models/IScriptResetInfo";
+import { DynamicScript, getDynamicScriptContent } from "/lib/system";
 
 export async function main(ns: NS): Promise<void> {
    const database = await Database.getInstance();
    await database.open();
 
-   // If it's only been 5 minutes since the last reset or aug install, clear object stores
-   const ResetInfo = await database.get<IScriptResetInfo>(DatabaseStoreName.NS_Data, "ns.getResetInfo");
-   const destroyedBitNodeRecently = Date.now() - ResetInfo.lastNodeReset < 300000;
-   const installedAugsRecently = Date.now() - ResetInfo.lastAugReset < 300000;
-   if (!ResetInfo || destroyedBitNodeRecently) {
-      ns.tprint("Resetting database after bitnode destruction");
-      await database.clear(DatabaseStoreName.Tasks);
-      await database.clear(DatabaseStoreName.Servers);
-      await database.clear(DatabaseStoreName.NS_Data);
-      await database.clear(DatabaseStoreName.Contracts);
-      await database.clear(DatabaseStoreName.Configuration);
+   await database.clear(DatabaseStoreName.Servers);
+   await database.clear(DatabaseStoreName.NS_Data);
 
-   } else if (installedAugsRecently) {
+   await database.saveRecord(DatabaseStoreName.Configuration, { key: "purchasedServersTask.js", value: { maxPower: 13 } });
+   await new DynamicScript("ns.getResetInfo", getDynamicScriptContent("ns.getResetInfo", "ns.getResetInfo()", DatabaseStoreName.NS_Data)).run(ns);
+
+   // If it"s only been 5 minutes since the last reset
+   const ResetInfo = await database.get<IScriptResetInfo>(DatabaseStoreName.NS_Data, "ns.getResetInfo");
+   if (ResetInfo.lastNodeReset < 300000) {
+      ns.tprint("Resetting database after bitnode destruction");
+   }
+
+   // If it"s only been 5 minutes since the last aug install
+   if (ResetInfo.lastAugReset < 300000) {
       ns.tprint("Resetting database after installing augments");
-      await database.clear(DatabaseStoreName.Servers);
-      await database.clear(DatabaseStoreName.NS_Data);
-      await database.clear(DatabaseStoreName.Contracts);
-      await database.clear(DatabaseStoreName.Configuration);
    }
 
    ns.run("daemon.js");
